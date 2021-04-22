628bb4766e27a1aea2b4bc5a88293540b6fe4cca
Marialice Freitas <marialice-freitas@hotmail.com>
2021-04-22 16:39:00 -0300

    testando array

--- collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
+++ collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -9,4 +9,22 @@ fun main() {
     values[3] = 4
     values[4] = 5
 
+    println("PRINTANDO COM FOR ...")
+    for (valor in values) { //imprime todos os valores do array
+        println(valor)
+    }
+
+
+    //o it é a nossa variável que o kotlin já mostra quando tem forEach
+    println("PRINTANDO COM FOR EACH ...")
+    values.forEach {
+        println(it)
+    }
+
+    println("PRINTANDO COM INDICES ...")
+    for (valor in values.indices) {
+        println(values[valor]) //recupera o valor que está naquela posição específica
+    }
+
+
 }
